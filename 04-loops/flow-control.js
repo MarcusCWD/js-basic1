c3f370f15e48a7db8935fde36cb716c2cee4fa7b
Marcus <marcuscey@gmail.com>
2022-01-27 08:35:10 +0000

    flow ontrol

---
+++ 04-loops/flow-control.js
@@ -0,0 +1,11 @@
+let n = 0;
+while(n<10) {
+    console.log(n);
+    if (n > 5) {
+        break; // exit the loop
+            // and go to the next line
+            // after the loop
+    }
+    n++;
+}
+console.log("loop ended");
