27f891fc42be0f8edff45e118312a9481d4ba5b8
Marcus <marcuscey@gmail.com>
2022-01-27 10:04:33 +0000

    pratice

--- 04-loops/flow-control.js
+++ 04-loops/flow-control.js
@@ -1,3 +1,4 @@
+// Break example
 let n = 0;
 while(n<10) {
     console.log(n);
@@ -9,3 +10,16 @@ while(n<10) {
     n++;
 }
 console.log("loop ended");
+
+
+// Continue example
+console.log("contuine example-----");
+for (let s = 10; s<21; s++) {
+    
+    if (s==15) {
+        continue;   //continue to the start
+                    // of the loop
+                    //will NOT go to console.log(s)
+    }
+    console.log(s);
+}
---
+++ 04-loops/practice.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+
+let isHungry = true
+let isSleepy = true
+if ((isHungry && isSleepy) === true) {
+    console.log("Good!");
+ }
